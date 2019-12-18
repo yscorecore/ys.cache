e5269ce7beaea7f4e22d9747589ac2dba4b42e85
root <root@TUNFI10DGNYHEF3.localdomain>
2019-12-18 23:21:29 +0800

    use lf char to new line in sh file

--- .github/scripts/pushpack.sh
+++ .github/scripts/pushpack.sh
@@ -2,6 +2,6 @@
 PUBLISH_FOLDER=$1
 NUGET_APIKEY=$2
 for file in `ls $PUBLISH_FOLDER/*.nupkg`
-do  
+do
     dotnet nuget push $file -k $NUGET_APIKEY -s  https://api.nuget.org/v3/index.json --skip-duplicate
 done        
