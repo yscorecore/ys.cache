289fbdd0f02d56db4042cd1e8c79aa89c4c5c34d
ypb <obpt@sina.com>
2019-12-18 20:54:19 +0800

    move script content to workflow file

--- .github/scripts/pushpack.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
- for file in ` ls $1/*.nupkg `  
-    do  
-        dotnet nuget push $file -k $2 -s  https://api.nuget.org/v3/index.json --skip-duplicate
-        
-    done  
-echo 'push package success.'
